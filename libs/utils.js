bd1a5ed6183628dff880a301fb1f2c32146f3ba5
SheikhArman100 <sheikharman100@gmail.com>
2023-08-06 13:28:24 +0600

    trycatch added to fix deployment problem

--- libs/utils.js
+++ libs/utils.js
@@ -1,72 +1,92 @@
-export const getRandomNumber=()=> {
+export const getRandomNumber = () => {
   // Generate a random number between 1 and 60 (both inclusive)
   return Math.floor(Math.random() * 60) + 1;
-}
+};
 export const getHeadline = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/headline`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const headline = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/headline`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const headline = await data.json();
 
-  return headline;
+    return headline;
+  } catch (error) {
+    console.log(error);
+  }
 };
 export const getNews = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/news`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const news = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/news`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const news = await data.json();
 
-  return news;
+    return news;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getOpinions = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/opinion`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const opinions = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/opinion`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const opinions = await data.json();
 
-  return opinions;
+    return opinions;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getCultures = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/culture`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const cultures = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/culture`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const cultures = await data.json();
 
-  return cultures;
+    return cultures;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getLifestyles = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/lifestyle`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const lifestyles = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/lifestyle`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const lifestyles = await data.json();
 
-  return lifestyles;
+    return lifestyles;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getSports = async () => {
-  const data = await fetch(`${process.env.HOST_URL}/api/sports`, {
-    next: {
-      revalidate: 21600000,
-    },
-  });
-  const sports = await data.json();
+  try {
+    const data = await fetch(`${process.env.HOST_URL}/api/sports`, {
+      next: {
+        revalidate: 21600000,
+      },
+    });
+    const sports = await data.json();
 
-  return sports;
+    return sports;
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-
-
-
